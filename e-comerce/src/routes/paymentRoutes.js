6f8a9a4e32cb5231f71c6bbb0f542371fc3493c0
Torres Rodrigo <kaioken1811@gmail.com>
2024-06-28 12:10:12 -0300

    Delete e-comerce/src directory

--- e-comerce/src/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = Router();
-const paymentController = require('../controllers/paymentController');
-const { ensureAuthenticated } = require('../middlewares/authMiddleware');
-
-router.get('/', ensureAuthenticated, paymentController.viewPayments);
-router.post('/process', ensureAuthenticated, paymentController.processPayment);
-
-module.exports = router;
